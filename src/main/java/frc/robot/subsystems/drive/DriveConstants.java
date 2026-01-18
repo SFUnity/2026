b68992aef04792e6c55cc8a5759e55b268563f41
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2026-01-17 23:01:22 -0800

    updated  gyroVisionLatencyLimit

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -101,7 +101,8 @@ public class DriveConstants {
   public static final double driveKs;
   public static final double driveKv;
 
-  public static final double gyroVisionLatencyLimit = 0.01;
+  //Time between Vision reading and gyro update in seconds
+  public static final double gyroVisionLatencyLimit = 0.05; //TODO tune to make sure it wont fuck up gyro but will actually let readings in
 
   static {
     switch (Constants.currentMode) {
