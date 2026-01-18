f41cf7110198bf215cf740d7a9170359e0cf1938
Lucas Lee <llee28@urbanschool.org>
2026-01-18 14:03:25 -0800

    add roller subsystem, create IO

---
+++ src/main/java/frc/robot/subsystems/rollers/GenericRollerSystemIO.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems.rollers;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface GenericRollerSystemIO {
+    @AutoLog
+    public static class GenericRollerInputs{
+        public double appliedVolts = 0.0;
+        public double currentAmps = 0.0;
+    }
+    public default void updateInputs(GenericRollerInputs inputs){}
+    public default void runVolts(double volts){}
+    public default void stop(){}
+}
