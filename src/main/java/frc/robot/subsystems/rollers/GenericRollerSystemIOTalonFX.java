f574c1fc746cca59dcea4e77a9872b0b561d12b2
Lucas Lee <llee28@urbanschool.org>
2026-01-18 14:20:03 -0800

    rollers talonfx+ format

--- src/main/java/frc/robot/subsystems/rollers/GenericRollerSystemIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/rollers/GenericRollerSystemIOTalonFX.java
@@ -1,3 +1,23 @@
 package frc.robot.subsystems.rollers;
 
-public class GenericRollerSystemIOTalonFX {}
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.NeutralOut;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+
+public class GenericRollerSystemIOTalonFX implements GenericRollerSystemIO {
+  private final TalonFX talon;
+  private final VoltageOut voltageOut = new VoltageOut(0).withEnableFOC(true).withUpdateFreqHz(0);
+  private final NeutralOut neutralOut = new NeutralOut();
+
+  public GenericRollerSystemIOTalonFX(int id, int currentLimitAmps, boolean invert) {
+    talon = new TalonFX(id);
+    TalonFXConfiguration config = new TalonFXConfiguration();
+    config.MotorOutput.Inverted =
+        invert ? InvertedValue.Clockwise_Positive : InvertedValue.CounterClockwise_Positive;
+    config.CurrentLimits.SupplyCurrentLimit = currentLimitAmps;
+    config.CurrentLimits.SupplyCurrentLimitEnable = true;
+    talon.getConfigurator().apply(config);
+  }
+}
