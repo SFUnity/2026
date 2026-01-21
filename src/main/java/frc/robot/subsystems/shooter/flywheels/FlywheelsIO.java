2cac239ed64df360d0f221a34b59ea7ac68dfda2
MINAJECHUNG <mchung27@urbanschool.org>
2026-01-20 18:30:59 -0800

    finished flywheelsIO

--- src/main/java/frc/robot/subsystems/shooter/flywheels/FlywheelsIO.java
+++ src/main/java/frc/robot/subsystems/shooter/flywheels/FlywheelsIO.java
@@ -2,6 +2,15 @@ package frc.robot.subsystems.shooter.flywheels;
 
 public interface FlywheelsIO {
     public static class FlywheelsIOOutputs{
-        
+        public double topVelocityRotsPerSec = 0.0;
+        public double topAppliedVolts = 0.0;
+        public double topCurrentAmps = 0.0;
+
+        public double bottomVelocityRotsPerSec = 0.0;
+        public double bottomAppliedVolts = 0.0;
+        public double bottomCurrentAmps = 0.0;
     }
+    public default void updateInputs(){}
+
+    public default void runVolts(){}
 }
