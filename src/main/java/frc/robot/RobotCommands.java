9df6a1da16538f412135b91275ba3c8121351191
Ishaan Shrimali <ishaan1003@gmail.com>
2026-01-24 12:54:10 -0800

    Deleted Cmds from IntakePivot

--- src/main/java/frc/robot/RobotCommands.java
+++ src/main/java/frc/robot/RobotCommands.java
@@ -4,14 +4,15 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public class RobotCommands {
-    public static Command intake() {
-        return Commands.none();
-    }
+  public static Command intake() {
+    return Commands.none();
+  }
 
-    public static Command climb() {
-        return Commands.none();
-    }
-    public static Command score() {
-        return Commands.none();
-    }
+  public static Command climb() {
+    return Commands.none();
+  }
+
+  public static Command score() {
+    return Commands.none();
+  }
 }
