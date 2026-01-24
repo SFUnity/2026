47bf036453d7294aa041feb4100c6ad4cf8df7ae
MINAJECHUNG <mchung27@urbanschool.org>
2026-01-24 12:00:38 -0800

    create flywheel subsystem

--- src/main/java/frc/robot/subsystems/shooter/flywheels/FlywheelsIOSim.java
+++ src/main/java/frc/robot/subsystems/shooter/flywheels/FlywheelsIOSim.java
@@ -1,5 +1,3 @@
 package frc.robot.subsystems.shooter.flywheels;
 
-public class FlywheelsIOSim {
-    
-}
+public class FlywheelsIOSim {}
--- src/main/java/frc/robot/subsystems/shooter/flywheels/FlywheelsIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/shooter/flywheels/FlywheelsIOTalonFX.java
@@ -4,51 +4,49 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.google.errorprone.annotations.OverridingMethodsMustInvokeSuper;
 
-public class FlywheelsIOTalonFX implements FlywheelsIO{
-    private final TalonFX talonFX;
-    private final VoltageOut voltageOut = new VoltageOut(0).withEnableFOC(true).withUpdateFreqHz(0);
-    private final MotionMagicVelocityVoltage motionMagicVelocity = new MotionMagicVelocityVoltage(0).withEnableFOC(true);
-    public FlywheelsIOTalonFX(double kV, double kP, double kD, int id){
-        talonFX = new TalonFX(id);
-        var talonFXConfigs = new TalonFXConfiguration();
+public class FlywheelsIOTalonFX implements FlywheelsIO {
+  private final TalonFX talonFX;
+  private final VoltageOut voltageOut = new VoltageOut(0).withEnableFOC(true).withUpdateFreqHz(0);
+  private final MotionMagicVelocityVoltage motionMagicVelocity =
+      new MotionMagicVelocityVoltage(0).withEnableFOC(true);
 
-        var slot0Configs = talonFXConfigs.Slot0;
-        slot0Configs.kS = 0; 
-        slot0Configs.kV = kV; 
-        slot0Configs.kA = 0.0; 
-        slot0Configs.kP = kP; 
-        slot0Configs.kI = 0; 
-        slot0Configs.kD = kD; 
+  public FlywheelsIOTalonFX(double kV, double kP, double kD, int id) {
+    talonFX = new TalonFX(id);
+    var talonFXConfigs = new TalonFXConfiguration();
 
-        // set Motion Magic settings
-        var motionMagicConfigs = talonFXConfigs.MotionMagic;
-        motionMagicConfigs.MotionMagicCruiseVelocity = 80; // Target cruise velocity of 80 rps
-        motionMagicConfigs.MotionMagicAcceleration = 160; // Target acceleration of 160 rps/s (0.5 seconds)
-        motionMagicConfigs.MotionMagicJerk = 1600; // Target jerk of 1600 rps/s/s (0.1 seconds)
+    var slot0Configs = talonFXConfigs.Slot0;
+    slot0Configs.kS = 0;
+    slot0Configs.kV = kV;
+    slot0Configs.kA = 0.0;
+    slot0Configs.kP = kP;
+    slot0Configs.kI = 0;
+    slot0Configs.kD = kD;
 
-        talonFX.getConfigurator().apply(talonFXConfigs);
+    // set Motion Magic settings
+    var motionMagicConfigs = talonFXConfigs.MotionMagic;
+    motionMagicConfigs.MotionMagicCruiseVelocity = 80; // Target cruise velocity of 80 rps
+    motionMagicConfigs.MotionMagicAcceleration =
+        160; // Target acceleration of 160 rps/s (0.5 seconds)
+    motionMagicConfigs.MotionMagicJerk = 1600; // Target jerk of 1600 rps/s/s (0.1 seconds)
 
-        
-    }
-    @Override
-    public void updateInputs(FlywheelsIOOutputs inputs){
-        inputs.appliedVolts = talonFX.getMotorVoltage().getValueAsDouble();
-        inputs.currentAmps = talonFX.getSupplyCurrent().getValueAsDouble();
-        inputs.velocityRotsPerSec = talonFX.getVelocity().getValueAsDouble();
-    }
+    talonFX.getConfigurator().apply(talonFXConfigs);
+  }
 
-    @Override 
-    public void runVelocity(double velocity){
-        talonFX.setControl(motionMagicVelocity.withVelocity(velocity));
-    }
+  @Override
+  public void updateInputs(FlywheelsIOInputs inputs) {
+    inputs.appliedVolts = talonFX.getMotorVoltage().getValueAsDouble();
+    inputs.currentAmps = talonFX.getSupplyCurrent().getValueAsDouble();
+    inputs.velocityRotsPerSec = talonFX.getVelocity().getValueAsDouble();
+  }
 
-    public void idle(){
-       talonFX.setControl(voltageOut.withOutput(0.0)); 
-    }
+  @Override
+  public void runVelocity(double velocity) {
+    talonFX.setControl(motionMagicVelocity.withVelocity(velocity));
+  }
 
-
-
-
+  @Override
+  public void idle() {
+    talonFX.setControl(voltageOut.withOutput(0.0));
+  }
 }
