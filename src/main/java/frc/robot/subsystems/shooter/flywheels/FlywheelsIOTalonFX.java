f9af2f9eef90f417f2146b58de4c023bb30bc525
MINAJECHUNG <mchung27@urbanschool.org>
2026-01-24 13:30:20 -0800

    flywheel sim

--- src/main/java/frc/robot/subsystems/shooter/flywheels/FlywheelsIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/shooter/flywheels/FlywheelsIOTalonFX.java
@@ -1,27 +1,30 @@
 package frc.robot.subsystems.shooter.flywheels;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
 
 public class FlywheelsIOTalonFX implements FlywheelsIO {
-  private final TalonFX talonFX;
+  private final TalonFX leader = new TalonFX(0);
+  private final TalonFX follow = new TalonFX(0);
   private final VoltageOut voltageOut = new VoltageOut(0).withEnableFOC(true).withUpdateFreqHz(0);
   private final MotionMagicVelocityVoltage motionMagicVelocity =
       new MotionMagicVelocityVoltage(0).withEnableFOC(true);
 
-  public FlywheelsIOTalonFX(double kV, double kP, double kD, int id) {
-    talonFX = new TalonFX(id);
+  public FlywheelsIOTalonFX() {
     var talonFXConfigs = new TalonFXConfiguration();
 
     var slot0Configs = talonFXConfigs.Slot0;
     slot0Configs.kS = 0;
-    slot0Configs.kV = kV;
+    slot0Configs.kV = 0;
     slot0Configs.kA = 0.0;
-    slot0Configs.kP = kP;
+    slot0Configs.kP = 0;
     slot0Configs.kI = 0;
-    slot0Configs.kD = kD;
+    slot0Configs.kD = 0;
 
     // set Motion Magic settings
     var motionMagicConfigs = talonFXConfigs.MotionMagic;
@@ -30,23 +33,24 @@ public class FlywheelsIOTalonFX implements FlywheelsIO {
         160; // Target acceleration of 160 rps/s (0.5 seconds)
     motionMagicConfigs.MotionMagicJerk = 1600; // Target jerk of 1600 rps/s/s (0.1 seconds)
 
-    talonFX.getConfigurator().apply(talonFXConfigs);
+    leader.getConfigurator().apply(talonFXConfigs);
+    follow.setControl(new Follower(leader.getDeviceID(), new MotorAlignmentValue(1)));
   }
 
   @Override
   public void updateInputs(FlywheelsIOInputs inputs) {
-    inputs.appliedVolts = talonFX.getMotorVoltage().getValueAsDouble();
-    inputs.currentAmps = talonFX.getSupplyCurrent().getValueAsDouble();
-    inputs.velocityRotsPerSec = talonFX.getVelocity().getValueAsDouble();
+    inputs.appliedVolts = leader.getMotorVoltage().getValueAsDouble();
+    inputs.currentAmps = leader.getSupplyCurrent().getValueAsDouble();
+    inputs.velocityRotsPerSec = leader.getVelocity().getValueAsDouble();
   }
 
   @Override
   public void runVelocity(double velocity) {
-    talonFX.setControl(motionMagicVelocity.withVelocity(velocity));
+    leader.setControl(motionMagicVelocity.withVelocity(velocity));
   }
 
   @Override
   public void idle() {
-    talonFX.setControl(voltageOut.withOutput(0.0));
+    leader.setControl(voltageOut.withOutput(0.0));
   }
 }
