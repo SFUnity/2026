10936eb75fda802fd406b136d3a638156806dfc0
Gabkwo <kwongabriel2010@gmail.com>
2026-01-25 13:49:12 -0800

    delete copy of scoreCenterClimb

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -138,14 +138,5 @@ public class Autos {
     routine.active().onTrue(RobotCommands.sequence(DepotFeed.resetOdometry(), DepotFeed.cmd()));
     DepotFeed.atTime("StartIntake").onTrue(RobotCommands.intake());
     DepotFeed.atTime("StopIntake").onTrue(RobotCommands.stopIntake());
-
-}
-
-public AutoRoutine ScoreCenterClimbAutoRoutine() {
-  AutoRoutine routine = factory.newRoutine("Score Center Climb Auto Routine");
-  AutoTrajectory ScoreCenterClimb = routine.trajectory("ScoreCenterClimb");
-  routine.active().onTrue(RobotCommands.sequence(ScoreCenterClimb.resetOdometry(), ScoreCenterClimb.cmd()));
-  ScoreCenterClimb.atTime("ExtendClimber").onTrue(RobotCommands.climbExtend());
-  ScoreCenterClimb.done().onTrue(RobotCommands.climbRetract());
-  return routine;
+  }
 }
