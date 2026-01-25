26edd94ff8e7b6e53845ec781ff34049c1df441a
Gabkwo <kwongabriel2010@gmail.com>
2026-01-25 13:26:08 -0800

    add extendClimber event marker for ScoreCenterClimb

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -109,5 +109,13 @@ public class Autos {
     Depot.done().onTrue(RobotCommands.climbRetract());
     return routine;
   }
-  
+
+  public AutoRoutine ScoreCenterClimbAutoRoutine() {
+    AutoRoutine routine = factory.newRoutine("ScoreCenterClimb Auto Routine");
+    AutoTrajectory ScoreCenterClimb = routine.trajectory("ScoreCenterClimb");
+    routine.active().onTrue(RobotCommands.sequence(ScoreCenterClimb.resetOdometry(), ScoreCenterClimb.cmd()));
+    ScoreCenterClimb.atTime("ExtendClimber").onTrue(RobotCommands.climbExtend());
+    ScoreCenterClimb.done().onTrue(RobotCommands.climbRetract());
+    return routine;
+  }
 }
