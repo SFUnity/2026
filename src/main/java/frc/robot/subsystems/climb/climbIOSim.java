05358cc23777969fee01b2fb9aefa9a95c10322b
TheDeadChicken <mishi.doron@gmail.com>
2026-01-28 08:51:56 -0800

    Setup ClmbIOSim

--- src/main/java/frc/robot/subsystems/climb/climbIOSim.java
+++ src/main/java/frc/robot/subsystems/climb/climbIOSim.java
@@ -1,5 +1,18 @@
 package frc.robot.subsystems.climb;
 
-public class climbIOSim {
+public class climbIOSim implements climbIO {
 
+  private double appliedVolts = 0;
+
+  public climbIOSim() {}
+
+  @Override
+  public void updateInputs(climbIOInputs inputs) {
+    inputs.appliedVolts = appliedVolts;
+  }
+
+  @Override
+  public void runVolts(double voltage) {
+    appliedVolts = voltage;
+  }
 }
