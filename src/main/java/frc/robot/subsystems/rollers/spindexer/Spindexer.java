bc4851179d77c58f98e611e8d9c9952c893d89e0
TheDeadChicken <mishi.doron@gmail.com>
2026-01-27 17:15:57 -0800

    Add runSpindexer and stopSpindexer commands

--- src/main/java/frc/robot/subsystems/rollers/spindexer/Spindexer.java
+++ src/main/java/frc/robot/subsystems/rollers/spindexer/Spindexer.java
@@ -1,12 +1,26 @@
 package frc.robot.subsystems.rollers.spindexer;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Spindexer extends SubsystemBase {
 
   private SpindexerIO io;
+  private final SpindexerIOInputsAutoLogged inputs = new SpindexerIOInputsAutoLogged();
 
-    public Spindexer(SpindexerIO io) {
-        this.io = io;
-    }
+  public Spindexer(SpindexerIO io) {
+    this.io = io;
+  }
+
+  public void periodic() {
+    io.updateInputs(inputs);
+  }
+
+  public Command runSpindexer(double speed) {
+    return run(() -> io.runVolts(speed));
+  }
+
+  public Command stopSpindexer() {
+    return run(() -> io.runVolts(0));
+  }
 }
