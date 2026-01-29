7143d23d1d6b3ef0303059c4140938dc8a2ebf0d
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2026-01-28 17:49:51 -0800

    Shooter with individual subsystems

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,7 +1,18 @@
 package frc.robot.subsystems.shooter;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.shooter.flywheels.Flywheels;
+import frc.robot.subsystems.shooter.hood.Hood;
+import frc.robot.subsystems.shooter.turret.Turret;
 
 public class Shooter extends SubsystemBase {
-    
+    private final Flywheels flywheels;
+    private final Turret turret;
+    private final Hood hood;
+
+    public Shooter(Flywheels flywheels, Turret turret, Hood hood) {
+        this.flywheels = flywheels;
+        this.turret = turret;
+        this.hood = hood;
+    }
 }
