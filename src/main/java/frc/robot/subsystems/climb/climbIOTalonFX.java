59f581e2e05043d83c78f3f19fcd5f940cc4a983
TheDeadChicken <mishi.doron@gmail.com>
2026-01-28 18:14:04 -0800

    Setup PIDs

--- src/main/java/frc/robot/subsystems/climb/climbIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/climb/climbIOTalonFX.java
@@ -2,16 +2,35 @@ package frc.robot.subsystems.climb;
 
 import static frc.robot.Constants.loopPeriodSecs;
 
+import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+
 
 public class ClimbIOTalonFX implements ClimbIO {
   private final TalonFX talon = new TalonFX(0);
 
-  private final VoltageOut voltageOut =
-      new VoltageOut(0).withEnableFOC(true).withUpdateFreqHz(loopPeriodSecs);
+  public ClimbIOTalonFX() {
+    var talonFXConfigs = new TalonFXConfiguration();
 
-  @Override
+    var slot0Configs = talonFXConfigs.Slot0;
+    slot0Configs.kP = 0;
+    slot0Configs.kI = 0;
+    slot0Configs.kD = 0;
+    talonFXConfigs.CurrentLimits.StatorCurrentLimit = 80.0;
+    talonFXConfigs.CurrentLimits.StatorCurrentLimitEnable = true;
+    talonFXConfigs.CurrentLimits.SupplyCurrentLimit = 60.0;
+    talon.getConfigurator().apply(talonFXConfigs);
+  }
+
+  // private final VoltageOut voltageOut =
+  //    new VoltageOut(0).withEnableFOC(true).withUpdateFreqHz(loopPeriodSecs);
+  final PositionVoltage positionVoltage = new PositionVoltage(0).withSlot(0);
+
+
+  @
+  Override
   public void updateInputs(climbIOInputs inputs) {
     inputs.appliedVolts = talon.getMotorVoltage().getValueAsDouble();
     inputs.statorCurrentAmps = talon.getStatorCurrent().getValueAsDouble();
@@ -20,7 +39,7 @@ public class ClimbIOTalonFX implements ClimbIO {
   }
 
   @Override
-  public void runVolts(double voltage) {
-    talon.setControl(voltageOut.withOutput(voltage));
+  public void setPosition(double rotations) {
+    talon.setControl(positionVoltage.withPosition(rotations));
   }
 }
