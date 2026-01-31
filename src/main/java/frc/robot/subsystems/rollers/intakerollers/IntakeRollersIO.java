c5fb690e1fd31bf7f0320b633ed9625a2ce758db
Lucas Lee <llee28@urbanschool.org>
2026-01-31 14:19:24 -0800

    rename variables in intake rollers io

--- src/main/java/frc/robot/subsystems/rollers/intakerollers/IntakeRollersIO.java
+++ src/main/java/frc/robot/subsystems/rollers/intakerollers/IntakeRollersIO.java
@@ -4,13 +4,13 @@ import org.littletonrobotics.junction.AutoLog;
 
 public interface IntakeRollersIO {
   @AutoLog
-  public static class IntakeIOInputs {
+  public static class IntakeRollersIOInputs {
     public double appliedVolts = 0.0;
     public double currentAmps = 0.0;
     public double velocity = 0.0;
   }
 
-  public default void updateInputs(IntakeIOInputs inputs) {}
+  public default void updateInputs(IntakeRollersIOInputs inputs) {}
 
   public default void runVolts(double volts) {}
 
