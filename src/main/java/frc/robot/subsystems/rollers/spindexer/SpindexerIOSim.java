971e8d0f57219fb60504bef89f7f42e2c0632106
MrTinker64 <avi.micah@gmail.com>
2026-02-04 17:33:32 -0800

    remove unused

--- src/main/java/frc/robot/subsystems/rollers/spindexer/SpindexerIOSim.java
+++ src/main/java/frc/robot/subsystems/rollers/spindexer/SpindexerIOSim.java
@@ -1,8 +1,5 @@
 package frc.robot.subsystems.rollers.spindexer;
 
-// TODO please remove unused import
-import frc.robot.subsystems.rollers.spindexer.SpindexerIO.SpindexerIOInputs;
-
 public class SpindexerIOSim implements SpindexerIO {
 
   private double appliedVolts = 0;
