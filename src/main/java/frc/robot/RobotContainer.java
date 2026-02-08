ae199f30ccdbc76cfb5b4ef7f01cfecd5291d32c
MrTinker64 <avi.micah@gmail.com>
2026-02-08 13:25:16 -0800

    change when heading is reset

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -269,9 +269,10 @@ public class RobotContainer {
     // Switch to X pattern when X button is pressed
     controller.x().onTrue(Commands.runOnce(drive::stopWithX, drive));
 
-    // Reset gyro to 0° when B button is pressed
+    // Reset gyro to 0° when start or back button is pressed
     controller
-        .b()
+        .start()
+        .or(controller.back())
         .onTrue(
             Commands.runOnce(
                     () ->
