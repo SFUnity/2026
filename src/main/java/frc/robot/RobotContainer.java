d132640a68c0c1d9db82815b86fa83cc00fda402
MrTinker64 <avi.micah@gmail.com>
2026-02-08 15:48:13 -0800

    format

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -170,28 +170,36 @@ public class RobotContainer {
         hood = new Hood(new HoodIOSim());
         kicker = new Kicker(new KickerIOSim());
         shooter = new Shooter(flywheels, turret, hood, poseManager);
-        
+
         fuelSim.spawnStartingFuel(); // spawns fuel in the depots and neutral zone
         // Register a robot for collision with fuel
         fuelSim.registerRobot(
-                Units.inchesToMeters(27), // from left to right in meters
-                Units.inchesToMeters(28), // from front to back in meters
-                Units.inchesToMeters(5), // from floor to top of bumpers in meters
-                () -> poseManager.getPose(), // Supplier<Pose2d> of robot pose
-                () -> drive.getChassisSpeeds()); // Supplier<ChassisSpeeds> of field-centric chassis speeds
+            Units.inchesToMeters(27), // from left to right in meters
+            Units.inchesToMeters(28), // from front to back in meters
+            Units.inchesToMeters(5), // from floor to top of bumpers in meters
+            () -> poseManager.getPose(), // Supplier<Pose2d> of robot pose
+            () ->
+                drive
+                    .getChassisSpeeds()); // Supplier<ChassisSpeeds> of field-centric chassis speeds
 
         // Register an intake to remove fuel from the field as a rectangular bounding box
-        //fuelSim.registerIntake(
-                //minX, maxX, minY, maxY, // robot-centric coordinates for bounding box in meters
-                //shouldIntakeSupplier, // (optional) BooleanSupplier for whether the intake should be active at a given moment
-                //callback); // (optional) Runnable called whenever a fuel is intaked
+        // fuelSim.registerIntake(
+        // minX, maxX, minY, maxY, // robot-centric coordinates for bounding box in meters
+        // shouldIntakeSupplier, // (optional) BooleanSupplier for whether the intake should be
+        // active at a given moment
+        // callback); // (optional) Runnable called whenever a fuel is intaked
 
-        fuelSim.setSubticks(5); // sets the number of physics iterations to perform per 20ms loop. Default = 5
+        fuelSim.setSubticks(
+            5); // sets the number of physics iterations to perform per 20ms loop. Default = 5
 
-        fuelSim.start(); // enables the simulation to run (updateSim must still be called periodically)
-        fuelSim.stop(); // stops the simulation running (updateSim will do nothing until start is called again)
+        fuelSim
+            .start(); // enables the simulation to run (updateSim must still be called periodically)
+        fuelSim.stop(); // stops the simulation running (updateSim will do nothing until start is
+        // called again)
 
-        fuelSim.enableAirResistance(); // an additional drag force will be applied to fuel in physics update step
+        fuelSim
+            .enableAirResistance(); // an additional drag force will be applied to fuel in physics
+        // update step
         break;
 
       default:
