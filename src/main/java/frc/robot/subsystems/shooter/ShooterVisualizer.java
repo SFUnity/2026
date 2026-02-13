220d6b5cd05ab0be102017d6676dff218057e931
MrTinker64 <avi.micah@gmail.com>
2026-02-12 17:56:36 -0800

    start cleaning up shooter visualizer

--- src/main/java/frc/robot/subsystems/shooter/ShooterVisualizer.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterVisualizer.java
@@ -2,7 +2,9 @@ package frc.robot.subsystems.shooter;
 
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.util.Color8Bit;
 
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.mechanism.LoggedMechanism2d;
@@ -10,43 +12,46 @@ import org.littletonrobotics.junction.mechanism.LoggedMechanismLigament2d;
 import org.littletonrobotics.junction.mechanism.LoggedMechanismRoot2d;
 
 public class ShooterVisualizer {
-  private LoggedMechanism2d shooterMech;
-  private LoggedMechanismRoot2d shooterBase;
-  private LoggedMechanismLigament2d turret;
-  private LoggedMechanismLigament2d hood;
+  private final LoggedMechanism2d hoodMechanism;
+  private final LoggedMechanismRoot2d hoodRoot;
+  private final LoggedMechanism2d turretMechanism;
+  private final LoggedMechanismRoot2d turretRoot;
+  private final LoggedMechanismLigament2d turret;
+  private final LoggedMechanismLigament2d hood;
   private LoggedMechanismLigament2d shooterVisualizer;
   private double xCoord = 0; // TODO update
   private double yCoord = 0;
   private double zCoord = 0;
   private final String key;
 
+  private final double turretDiameter = Units.inchesToMeters(8);
+
   public ShooterVisualizer(String key, Color color) {
     this.key = key;
-    shooterMech = new LoggedMechanism2d(0, 0);
-    shooterBase = shooterMech.getRoot("Shooter Base", xCoord, yCoord);
-    turret = new LoggedMechanismLigament2d("Turret", 0, 0);
-    hood = new LoggedMechanismLigament2d("Hood", 0, 0);
-    shooterVisualizer = new LoggedMechanismLigament2d("Shooter Visualizer", 0, 0);
 
-    shooterBase.append(turret);
-    turret.append(hood);
-    hood.append(shooterVisualizer);
+    hoodMechanism = new LoggedMechanism2d(0, 0, new Color8Bit(Color.kBlack));
+    hoodRoot = hoodMechanism.getRoot("Shooter Root", xCoord, yCoord);
+    hood = new LoggedMechanismLigament2d("Hood", 0, 0);
+
+    turretMechanism = new LoggedMechanism2d(0, 0, new Color8Bit(Color.kBlack));
+    turretRoot = turretMechanism.getRoot("Turret Root", xCoord, yCoord);
+    turret = new LoggedMechanismLigament2d("Turret", turretDiameter, 0, 8,  new Color8Bit(color));
+
+    turretRoot.append(turret);
+    hoodRoot.append(hood);
   }
 
-  public void update(double turretAngle, double hoodAngle, double flywheelSpeed) {
+  public void update(double turretAngle, double hoodAngle) {
     turret.setAngle(turretAngle);
     hood.setAngle(hoodAngle);
-    shooterVisualizer.setLength(flywheelSpeed);
+    Logger.recordOutput("Subsystems/Shooter/Hood/Mechanism2D/" + key, hoodMechanism);
 
-    Logger.recordOutput("Subsystems/Shooter/Mechanism2D/" + key, shooterMech);
-
-    Pose3d ShooterPose =
+    Pose3d shooterPose =
         new Pose3d(
-            xCoord + Math.cos(turretAngle) * Math.cos(hoodAngle) * flywheelSpeed,
-            yCoord + Math.sin(turretAngle) * Math.cos(hoodAngle) * flywheelSpeed,
-            zCoord + Math.sin(hoodAngle) * flywheelSpeed,
+            xCoord + Math.cos(turretAngle) * Math.cos(hoodAngle),
+            yCoord + Math.sin(turretAngle) * Math.cos(hoodAngle),
+            zCoord + Math.sin(hoodAngle),
             new Rotation3d());
-
-    Logger.recordOutput("Subsystems/Shooter/Mechanism3D/" + key, ShooterPose);
+    Logger.recordOutput("Subsystems/Shooter/Mechanism3D/" + key, shooterPose);
   }
 }
